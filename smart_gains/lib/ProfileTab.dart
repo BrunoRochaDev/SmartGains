ab4ebf2d4e7248d4b94392014d8ebc64713b909e
rafael <rafael.lixa@ua.pt>
2022-10-13 10:46:39 +0100

    Starting User Profile

--- smart_gains/lib/ProfileTab.dart
+++ smart_gains/lib/ProfileTab.dart
@@ -35,7 +35,139 @@ class ProfileTab extends StatelessWidget {
               )
             ]),
           ),
-          Row()
+          Padding(
+              padding: const EdgeInsets.only(left: 40, right: 40, top: 64),
+              child: Row(
+                children: [
+                  Container(
+                    height: 260.0,
+                    width: 130.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.amber,
+                      shape: BoxShape.rectangle,
+                    ),
+                    child: Center(child: Text("TDEE, Calaries")),
+                  ),
+                  Expanded(child: Container()),
+                  Column(
+                    children: [
+                      Container(
+                        height: 120.0,
+                        width: 130.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.amber,
+                          shape: BoxShape.rectangle,
+                        ),
+                        child: Center(
+                            child: Text(
+                          "Current Weight: 200kg",
+                          textAlign: TextAlign.center,
+                        )),
+                      ),
+                      Container(
+                        height: 120.0,
+                        width: 130.0,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.amber,
+                          shape: BoxShape.rectangle,
+                        ),
+                        child: Center(
+                            child: Text(
+                          "Height: 1.80 m",
+                          textAlign: TextAlign.center,
+                        )),
+                      )
+                    ],
+                  )
+                ],
+              )),
+          Padding(
+            padding: const EdgeInsets.only(left: 40, right: 40, top: 32),
+            child: Container(
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                "My objectives",
+                textAlign: TextAlign.left,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+            ),
+          ),
+          Padding(
+              padding: const EdgeInsets.only(left: 40, right: 40, top: 32),
+              child: Container(
+                height: 200.0,
+                width: 200.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  image: DecorationImage(
+                    image: AssetImage('assets/weight-progress-chart.png'),
+                    fit: BoxFit.fitWidth,
+                  ),
+                  shape: BoxShape.rectangle,
+                ),
+              )),
+          Padding(
+            padding: const EdgeInsets.only(left: 40, right: 40, top: 32),
+            child: Row(
+              children: [
+                Container(
+                  height: 120.0,
+                  width: 130.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.amber,
+                    shape: BoxShape.rectangle,
+                  ),
+                  child: Center(
+                      child: Text(
+                    "Hours Trained this week 3/5 60%",
+                    textAlign: TextAlign.center,
+                  )),
+                ),
+                Expanded(child: Container()),
+                Container(
+                  height: 120.0,
+                  width: 130.0,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.amber,
+                    shape: BoxShape.rectangle,
+                  ),
+                  child: Center(
+                      child: Text(
+                    "Hours Trained this month 10/59 16%",
+                    textAlign: TextAlign.center,
+                  )),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 40, right: 40, top: 32),
+            child: Container(
+              alignment: Alignment.centerLeft,
+              child: Row(
+                children: [
+                  Text(
+                    "Daily Tasks",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                  ),
+                  Expanded(child: Container()),
+                  TextButton(
+                    onPressed: () {},
+                    child: Icon(Icons.edit),
+                    style: ButtonStyle(
+                        foregroundColor:
+                            MaterialStateProperty.all(Colors.black)),
+                  )
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     ));
