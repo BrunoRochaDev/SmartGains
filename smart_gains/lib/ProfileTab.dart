e055be9d8bf1ce8cffcadb5b5bbf12594c2a6fb5
rafael <rafael.lixa@ua.pt>
2022-10-13 09:54:53 +0100

    Fixed Train tab Scrollview

--- smart_gains/lib/ProfileTab.dart
+++ smart_gains/lib/ProfileTab.dart
@@ -6,8 +6,38 @@ class ProfileTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Text("Profile"),
+        body: SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 32, right: 32, top: 64),
+            child: Row(children: [
+              Expanded(
+                  child: Title(
+                color: Colors.black,
+                child: Text(
+                  "Persons Name",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+              )),
+              Container(
+                height: 130.0,
+                width: 130.0,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  image: DecorationImage(
+                    image: AssetImage('assets/womanImage.jpg'),
+                    fit: BoxFit.fitWidth,
+                  ),
+                  shape: BoxShape.rectangle,
+                ),
+              )
+            ]),
+          ),
+          Row()
+        ],
+      ),
     ));
   }
 }
