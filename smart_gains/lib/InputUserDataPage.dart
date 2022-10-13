26366b18bd42651bf50d4c25c556341d79a1d20f
icbaptista <ines.castro.batista@gmail.com>
2022-10-13 13:36:55 +0100

    post request input data

--- smart_gains/lib/InputUserDataPage.dart
+++ smart_gains/lib/InputUserDataPage.dart
@@ -1,6 +1,24 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 import 'FitnessGoalsPage.dart';
+
+const List<String> list = <String>[
+  '1 time per week',
+  '2 times per week',
+  '3 times per week',
+  '4 times per week',
+  '5 times per week'
+];
+
+String height = "";
+String weight = "";
+String gender = "";
+String dateOfBirth = "";
+String dailyGoal = "";
+final myController = TextEditingController();
 
 class InputUserData extends StatelessWidget {
   const InputUserData({Key? key, required this.title}) : super(key: key);
@@ -111,6 +129,27 @@ class InputUserData extends StatelessWidget {
                   Padding(
                     padding:
                         const EdgeInsets.only(left: 32, right: 32, bottom: 32),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(20)),
+                              prefixIcon: const Icon(Icons.transgender),
+                              hintText: 'Daily Goal',
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: DropdownButtonExample(),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 32, right: 32, bottom: 32),
                     child: ElevatedButton(
                         style: ElevatedButton.styleFrom(
                             fixedSize: const Size(320, 40),
@@ -118,6 +157,7 @@ class InputUserData extends StatelessWidget {
                             backgroundColor:
                                 const Color.fromARGB(255, 37, 171, 117)),
                         onPressed: () {
+                          fetch();
                           Navigator.push(
                             context,
                             MaterialPageRoute(
@@ -133,6 +173,50 @@ class InputUserData extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+void fetch() async {
+  final response = await http.post(
+      Uri.parse('http://192.168.180.8:8393/user?username=filipe%27'),
+      body: jsonEncode(<String, String>{}));
+  print(jsonDecode(response.body));
+}
+
+class DropdownButtonExample extends StatefulWidget {
+  const DropdownButtonExample({super.key});
+
+  @override
+  State<DropdownButtonExample> createState() => _DropdownButtonExampleState();
+}
+
+class _DropdownButtonExampleState extends State<DropdownButtonExample> {
+  String dropdownValue = list.first;
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton<String>(
+      value: dropdownValue,
+      icon: const Icon(Icons.arrow_downward),
+      elevation: 16,
+      style: const TextStyle(color: Colors.deepPurple),
+      underline: Container(
+        height: 2,
+        color: Colors.deepPurpleAccent,
+      ),
+      onChanged: (String? value) {
+        // This is called when the user selects an item.
+        setState(() {
+          dropdownValue = value!;
+        });
+      },
+      items: list.map<DropdownMenuItem<String>>((String value) {
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value),
+        );
+      }).toList(),
     );
   }
 }
