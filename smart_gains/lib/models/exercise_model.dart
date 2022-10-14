17b8f4fb3eb391bce04f615d9464f39411691691
jteles277 <59253209+jteles277@users.noreply.github.com>
2022-10-14 01:02:43 +0100

    model correction

--- smart_gains/lib/models/exercise_model.dart
+++ smart_gains/lib/models/exercise_model.dart
@@ -29,7 +29,7 @@ final Exercise deadLift = Exercise(
         '1. Stand with your mid-foot under the barbell\n\n2. Bend over and grab the bar with a shoulder-width grip\n\n3.Bend your knees until your shins touch the bar\n\n4. Lift your chest up and straighten your lower back\n\n5. Take a big breath, hold it, and stand up with the weight\n\n',
     instruction_image: 'assets/stronglifts-deadlift.webp',
     camera_positioning:
-        '1. Position yourself in front of the camera\n\n2. Make sure your whole body is catched by the camera\n\n3. To start, perform the motion in the gif\n\n4. To stop, perform the motion again.',
+        '1. Position yourself in front of the camera\n\n2. Make sure your whole body is caught by the camera\n\n3. To start, perform the motion in the gif\n\n4. To stop, perform the motion again.',
     starter_pose: "assets/wakanda_forever.gif");
 
 final Exercise squat = Exercise(
@@ -41,7 +41,7 @@ final Exercise squat = Exercise(
       "1. Stand with the bar on your upper-back, and your feet shoulder-width apart\n\n2. Squat down by pushing your knees to the side while moving hips back\n\n3. Break parallel by Squatting down until your hips are lower than your knees\n\n4. Squat back up while keeping your knees out and chest up\n\n 5.Stand with your hips and knees locked at the top\n\n",
   instruction_image: "assets/stronglifts-squat.webp",
   camera_positioning:
-      '1. To start, perform the motion in the gif\n\n2. To do the exercise, position yourself perpendicularly to the camera\n\n2. Make sure your whole body is catched by the camera\n\n4. To stop, perform the motion in the gif again.',
+      '1. To start, perform the motion in the gif\n\n2. To do the exercise, position yourself perpendicularly to the camera\n\n2. Make sure your whole body is caught by the camera\n\n4. To stop, perform the motion in the gif again.',
   starter_pose: "assets/wakanda_forever.gif",
 );
 
@@ -54,7 +54,7 @@ final Exercise pushup = Exercise(
       '1. Place your palms on the floor and ensure your thumbs are touching each other.\n\n2. Extend your legs backwards and make sure that only your toes are touching the floor.\n\n3. Maintain a straight body.\n\n4. Keep tension on your triceps by slightly bending your elbows.Lower your body as far as you can but don’t touch the ground.\n\n5. Ensure your body maintains a straight line.\n\n6. Pause and push back to the starting position without locking the elbows.',
   instruction_image: 'assets/gif_pushup.gif',
   camera_positioning:
-      '1. To start the rep counter, perform the motion in the gif\n\n2. To do the exercise, position yourself perpendicularly to the camera\n\n2. Make sure your whole body is catched by the camera\n\n4. To stop, perform the motion in the gif again.',
+      '1. To start the rep counter, perform the motion in the gif\n\n2. To do the exercise, position yourself perpendicularly to the camera\n\n2. Make sure your whole body is caught by the camera\n\n4. To stop, perform the motion in the gif again.',
   starter_pose: "assets/wakanda_forever.gif",
 );
 
@@ -67,7 +67,7 @@ final Exercise curl = Exercise(
         "1. Bend at the elbow, lifting the lower arms to pull the weights toward the shoulders.\n\n2. Your upper arms are stationary, and your wrists align with the forearms.\n\n3. Hold for one second at the top of the movement.\n\n4. Your thumbs will be close to the shoulders and palms facing toward the body's midline.\n\n5. Lower the weights to return to the starting position.",
     instruction_image: 'assets/bicep-curl.gif',
     camera_positioning:
-        '1. To start, perform the motion in the gif\n\n2. To do the exercise, position yourself perpendicularly to the camera\n\n2. Make sure your whole body is catched by the camera\n\n4. To stop, perform the motion in the gif again.',
+        '1. To start, perform the motion in the gif\n\n2. To do the exercise, position yourself perpendicularly to the camera\n\n2. Make sure your whole body is caught by the camera\n\n4. To stop, perform the motion in the gif again.',
     starter_pose: "assets/wakanda_forever.gif");
 
 List<Exercise> exercises = [deadLift, squat, pushup, curl];
